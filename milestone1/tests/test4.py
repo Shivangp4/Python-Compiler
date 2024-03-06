e83f15bb519263b52c82ae5992fd09d38387d770
Yash Gupta <yashbg7@gmail.com>
2024-03-07 04:28:10 +0530

    Update test4.py

--- milestone1/tests/test4.py
+++ milestone1/tests/test4.py
@@ -10,15 +10,31 @@ def binarySearch(array: list[int], x: int, low: int, high: int) -> int:
   return -1
 
 
+def linearSearch(array: list[int], x: int) -> int:
+  i: int = 0
+  for i in range(len(array)):
+    if array[i] == x:
+      return i
+  return -1
+
+
 def main():
   array: list[int] = [3, 4, 5, 6, 7, 8, 9]
-  result: int = binarySearch(array, 4, 0, len(array) - 1)
+  result1: int = binarySearch(array, 4, 0, len(array) - 1)
 
-  if result != -1:
-    print("Element is present at index:")
-    print(result)
+  if result1 != -1:
+    print("Binary search: element is present at index:")
+    print(result1)
   else:
-    print("Element is not present")
+    print("Binary search: element is not present")
+  
+  result2: int = linearSearch(array, 4)
+
+  if result2 != -1:
+    print("Linear search: element is present at index:")
+    print(result2)
+  else:
+    print("Linear search: element is not present")
 
 
 if __name__ == "__main__":
